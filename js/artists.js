1dc08e5cf6e0453b7577add3729c51d138e28d75
Joan Andrés <andres6936@live.com>
2020-04-09 09:06:07 -0500

    Version 2020.0409

---
+++ js/artists.js
@@ -0,0 +1,55 @@
+const artists = [
+    {
+        "name": "Adele",
+        "tracks": [
+            {
+                "name": "Hello",
+                "duration": 295,
+                "lyrics": "Hello, it's me \nI was wondering if after all these years \nYou'd like to meet, to go over everything \nThey say that time's supposed to heal ya \nBut I ain't done much healing\n \nHello, can you hear me? \nI'm in California dreaming about who we used to be \nWhen we were younger and free \nI've forgotten how it felt before the world fell at our feet \n\nThere's such a difference between us \nAnd a million miles \n\nHello from the other side \nI must've called a thousand times to tell you \nI'm sorry, for everything that I've done \nBut when I call you never seem to be home \n\nHello from the outside \nAt least I can say that I've tried to tell you \nI'm sorry, for breaking your heart \nBut it don't matter, it clearly doesn't tear you apart anymore \n\nHello, how are you? \nIt's so typical of me to talk about myself \nI'm sorry, I hope that you're well \nDid you ever make it out of that town \nWhere nothing ever happened? \n\nIt's no secret \nThat the both of us are running out of time \n\nSo hello from the other side \nI must've called a thousand times to tell you \nI'm sorry, for everything that I've done \nBut when I call you never seem to be home \n\nHello from the outside \nAt least I can say that I've tried to tell you \nI'm sorry, for breaking your heart \nBut it don't matter, it clearly doesn't tear you apart anymore \n\nOoooohh, anymore \nOoooohh, anymore \nOoooohh, anymore \nAnymore \n\nHello from the other side \nI must've called a thousand times to tell you \nI'm sorry, for everything that I've done \nBut when I call you never seem to be home \n\nHello from the outside \nAt least I can say that I've tried to tell you \nI'm sorry, for breaking your heart \nBut it don't matter, it clearly doesn't tear you apart anymore.",
+                "difficulty": 3
+            }
+        ]
+    },
+    {
+        "name": "Rihana",
+        "tracks": [
+            {}
+        ]
+    },
+    {
+        "name": "Michael Jackson",
+        "tracks": [
+            {}
+        ]
+    },
+    {
+        "name": "Green Day",
+        "tracks": [
+            {}
+        ]
+    },
+    {
+        "name": "AC/DC",
+        "tracks": [
+            {}
+        ]
+    },
+    {
+        "name": "Calvin Harris",
+        "tracks": [
+            {}
+        ]
+    },
+    {
+        "name": "Bon Jovi",
+        "tracks": [
+            {}
+        ]
+    },
+    {
+        "name": "ChocQuibTown",
+        "tracks": [
+            {}
+        ]
+    }
+];
