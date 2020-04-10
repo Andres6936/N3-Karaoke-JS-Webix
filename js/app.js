52512b50e0a12938d0223fd0894a19839ed98473
Joan Andrés <andres6936@live.com>
2020-04-10 18:24:09 -0500

    Separated panelPlay

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ import {panelOptionsTracks} from "./view/panelOptionsTracks.js";
 import {panelOptionsList} from "./view/panelOptionsList.js";
 import {panelProperty} from "./view/panelProperty.js";
 import {panelLyrics} from "./view/panelLyrics.js";
+import {panelPlay} from "./view/panelPlay.js";
 
 let appLayout = {
     width: 800,
@@ -26,22 +27,7 @@ let appLayout = {
                     rows: [
                         panelProperty,
                         panelLyrics,
-                        {
-                            cols: [
-                                {
-                                    view: "button", type: "icon", value: "Play",
-                                    icon: "wxi-menu-right", css: "webix_transparent"
-                                },
-                                {
-                                    view: "button", type: "icon", value: "Stop",
-                                    icon: "wxi-checkbox-blank", css: "webix_transparent"
-                                },
-                                {
-                                    view: "button", type: "icon", value: "Next",
-                                    icon: "wxi-angle-double-right", css: "webix_transparent"
-                                },
-                            ]
-                        }
+                        panelPlay
                     ]
                 }
             ]
---
+++ js/view/panelPlay.js
@@ -0,0 +1,18 @@
+let panelPlay = {
+    cols: [
+        {
+            view: "button", type: "icon", value: "Play",
+            icon: "wxi-menu-right", css: "webix_transparent"
+        },
+        {
+            view: "button", type: "icon", value: "Stop",
+            icon: "wxi-checkbox-blank", css: "webix_transparent"
+        },
+        {
+            view: "button", type: "icon", value: "Next",
+            icon: "wxi-angle-double-right", css: "webix_transparent"
+        },
+    ]
+};
+
+export {panelPlay}
