42e06e480f4001763edef28770acbaf472af7b83
Joan Andrés <andres6936@live.com>
2020-04-10 18:06:36 -0500

    Separated panelListTrack

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import {panelListTrack} from "./view/panelListTrack.js";
+
 let appLayout = {
     width: 800,
     minHeight: 600,
@@ -61,20 +63,7 @@ let appLayout = {
         // Third Row
         {
             cols: [
-                {
-                    borderless: true,
-                    view: "list",
-                    template: "#name# - #count#",
-                    select: true,
-                    data: [
-                        {id: 1, name: "The Shawshank Redemption", count: 1994},
-                        {id: 2, name: "The Godfather", count: 1972},
-                        {id: 3, name: "The Godfather: Part II", count: 1974},
-                        {id: 4, name: "The Good, the Bad and the Ugly", count: 1966},
-                        {id: 5, name: "My Fair Lady", count: 1964},
-                        {id: 6, name: "12 Angry Men", count: 1957}
-                    ]
-                },
+                panelListTrack,
                 {
                     rows: [
                         {view: "button", value: "Add List"},
@@ -97,3 +86,5 @@ let appLayout = {
         },
     ]
 };
+
+export {appLayout}
---
+++ js/view/panelListTrack.js
@@ -0,0 +1,16 @@
+let panelListTrack = {
+    borderless: true,
+    view: "list",
+    template: "#name# - #count#",
+    select: true,
+    data: [
+        {id: 1, name: "The Shawshank Redemption", count: 1994},
+        {id: 2, name: "The Godfather", count: 1972},
+        {id: 3, name: "The Godfather: Part II", count: 1974},
+        {id: 4, name: "The Good, the Bad and the Ugly", count: 1966},
+        {id: 5, name: "My Fair Lady", count: 1964},
+        {id: 6, name: "12 Angry Men", count: 1957}
+    ]
+};
+
+export {panelListTrack}
