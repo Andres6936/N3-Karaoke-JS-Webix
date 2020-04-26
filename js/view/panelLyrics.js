d48040e7afb02bf40c1dedeef6411e98fc0a943c
Joan Andrés <andres6936@live.com>
2020-04-26 14:30:52 -0500

    Added header to the component

--- js/view/panelLyrics.js
+++ js/view/panelLyrics.js
@@ -1,9 +1,20 @@
 let panelLyrics = {
-    id: "panelLyrics",
-    view: "textarea",
-    label: "Lyrics",
-    labelPosition: "top",
-    readonly: true,
+    rows: [
+        // Header of the component
+        // Reference: Adding Headers
+        // to Components (Webix Docs)
+        {
+            view: "template",
+            template: "Lyrics",
+            type: "section"
+        },
+        // Body of the component
+        {
+            id: "panelLyrics",
+            view: "textarea",
+            readonly: true,
+        }
+    ]
 };
 
 export {panelLyrics}
